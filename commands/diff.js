80edd1b6f4d2ba1953ff779a3b06cc4af8cf8494
Maksim Lin <maks@manichord.com>
2014-02-13 21:52:14 +1100

    unified diff rendering using jsdiff module from npm
    jsdiff from: https://www.npmjs.org/package/diff
    npm version 1.0.8

--- commands/diff.js
+++ commands/diff.js
@@ -1,4 +1,4 @@
-define(["utils/misc_utils"], function (utils) {
+define(['thirdparty/jsdiff', 'utils/misc_utils'], function (_na_, utils) { //JsDiff is a global, no AMD module
   var diffTree = function(oldTree, newTree){
 		oldTree.sortEntries();
 		newTree.sortEntries();
@@ -44,7 +44,41 @@ define(["utils/misc_utils"], function (utils) {
 		}
 		return {add:add, remove:remove, merge: merge};
 	};
-    return {
-		diffTree : diffTree
+	
+	
+	/**
+	 * Show a Diff for the given 2 trees, recursing down through all subtrees
+	 * TODO: the recursing bit !!!
+	 */
+	var renderDiff = function(treeA, treeB, store) {
+		var result = diffTree(treeA, treeB);
+		console.log("Render Diff", treeA, treeB);
+		function processDiffList(objList) {
+			console.log("objList:", objList);
+			if (objList.length == 2) {
+				console.log(JsDiff.createPatch(name, objList[0].data, objList[1].data));
+			} else {
+				console.error("Did not find both Blob objects for SHA's:", objList);
+			}
+		}
+		
+		console.log("Diff Result:", result);
+		for (var i=0; i < result.merge.length; i++) {
+			console.log("modified entries:", result.merge[i]);
+			if (result.merge[i].old.isBlob) {
+				var shaA = result.merge[i].old.sha;
+				var shaB = result.merge[i].nu.sha;
+				var name = result.merge[i].old.name;
+				var gitDiffPrefix = utils.convertBytesToSha(shaA).substr(0, 7)+".."+utils.convertBytesToSha(shaB).substr(0, 7);
+				store._retrieveBlobsAsStrings([shaA, shaB], processDiffList);
+			} else {
+				console.log("modified is tree:", result.merge[i]);
+			}
+		}
+	};
+	
+	return {
+		diffTree : diffTree,
+		renderDiff: renderDiff
 	}
 });
