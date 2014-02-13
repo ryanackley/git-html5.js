b886efc5c74329913dca19cf16f0c7ad5363dd67
Maksim Lin <maks@manichord.com>
2014-02-13 13:22:36 +1100

    doh! use the existing diffTree function
    well turns out that there already was an existing diffTree function
    pretty much identical (though more elegant code) to the one I wrote
    so just using that but refactoring it out into its own file.

--- commands/diff.js
+++ commands/diff.js
@@ -1,70 +1,50 @@
 define(["utils/misc_utils"], function (utils) {
-  return {
-    /**
-     * takes 2 git-html5 tree objs, which have an entries prop Array
-     * and creates a list of differences between the 2
-     * @return a object with 3 props: added, deleted, modified, each containing the name of the tree entry
-     * NOTE: that modified is an array where each item is a array of exactly 2 tree entries which have same
-     * path name but differing SHA's.
-     */
-    diffTrees: function (treeA, treeB){
-      console.log("diffing trees:", treeA, treeB);
-      var diffLists = {
-          added : [],
-          deleted : [],
-          modified : []
-      };
-      var inBoth;
-      var treeBModEntry;
-
-      diffLists.added = _removeArrayItems(treeB.entries, treeA.entries);
-      diffLists.deleted = _removeArrayItems(treeA.entries, treeB.entries);
-      inBoth = _removeArrayItems(treeA.entries, diffLists.deleted);
-      console.log("both", inBoth);
-
-      for(var i=0; i < inBoth.length; i++) {
-        treeBModEntry = _getTreeEntry(treeB, inBoth[i].name);
-        if (utils.compareShas(treeBModEntry.sha, inBoth[i].sha) != 0) {
-          if(inBoth[i].isBlob) {
-            diffLists.modified.push([inBoth[i], treeBModEntry]);
-          }
-        }
-      }
-      return diffLists;
-    }
-  };
-
-  //return entry in tree matching name or null
-  function _getTreeEntry(tree, name) {
-      var result = null;
-      tree.entries.some(function(b) {
-          if (b.name == name) {
-              result = b;
-              return true;
-          }
-      });
-      return result;
-  }
-
-  /*
-   * Removes a set of strings from an array. Takes two arrays as parameters, 
-   * first being the array of items you want to manipulate, second a Array of items
-   * you want to remove from the original array.
-   * src: http://www.bytechaser.com/en/functions/cutxqjhtxq/remove-items-from-a-javascript-array.aspx
-   *
-   * @returns A new Array with all the selected items removed
-   */
-  function _removeArrayItems(array, itemsToRemove) {
-    if (array.length == 0 || itemsToRemove.length == 0){
-      return array;
-    }
-    var sMatchedItems = "|"+itemsToRemove.join('|')+"|";
-    var newArray = [];
-    for(var i=0; i<array.length; i++) {
-        if(sMatchedItems.indexOf("|"+array[i]+"|") < 0) {
-            newArray[newArray.length]=array[i];
-        }
-    }
-    return newArray;
-  }
+  var diffTree = function(oldTree, newTree){
+		oldTree.sortEntries();
+		newTree.sortEntries();
+		
+		var oldEntries = oldTree.entries, 
+			newEntries = newTree.entries;
+		var oldIdx = newIdx = 0;
+		
+		var remove = [],
+			add = [], 
+			merge = [];
+		
+		while (true){
+			var nu = newEntries[newIdx];
+			var old = oldEntries[oldIdx];
+			
+			if (!nu){
+				if (!old){
+					break;
+				}
+				remove.push(old);
+				oldIdx++;
+			}
+			else if (!old){
+				add.push(nu);
+				newIdx++;
+			}
+			else if (nu.name < old.name){
+				add.push(nu);
+				newIdx++;
+			}
+			else if (nu.name > old.name){
+				remove.push(old);
+				oldIdx++;
+			}
+			else{
+				if (utils.compareShas(nu.sha,old.sha) != 0){
+					merge.push({nu:nu, old:old});
+				}
+				oldIdx++;
+				newIdx++;
+			}
+		}
+		return {add:add, remove:remove, merge: merge};
+	};
+    return {
+		diffTree : diffTree
+	}
 });
--- commands/treemerger.js
+++ commands/treemerger.js
@@ -1,53 +1,5 @@
 define(['utils/misc_utils'], function(utils){
 
-	var diffTree = function(oldTree, newTree){
-		oldTree.sortEntries();
-		newTree.sortEntries();
-		
-		var oldEntries = oldTree.entries, 
-			newEntries = newTree.entries;
-		var oldIdx = newIdx = 0;
-		
-		var remove = [],
-			add = [], 
-			merge = [];
-		
-		
-		
-		while (true){
-			var nu = newEntries[newIdx];
-			var old = oldEntries[oldIdx];
-			
-			if (!nu){
-				if (!old){
-					break;
-				}
-				remove.push(old);
-				oldIdx++;
-			}
-			else if (!old){
-				add.push(nu);
-				newIdx++;
-			}
-			else if (nu.name < old.name){
-				add.push(nu);
-				newIdx++;
-			}
-			else if (nu.name > old.name){
-				remove.push(old);
-				oldIdx++;
-			}
-			else{
-				if (utils.compareShas(nu.sha,old.sha) != 0){
-					merge.push({nu:nu, old:old});
-				}
-				oldIdx++;
-				newIdx++;
-			}
-		}
-		return {add:add, remove:remove, merge: merge};
-	};
-	
 	 var mergeTrees = function(store, ourTree, baseTree, theirTree, success, error){
 		
 		var finalTree = [], 
@@ -215,8 +167,6 @@ define(['utils/misc_utils'], function(utils){
 		
 	}
 	return {
-		mergeTrees : mergeTrees,
-		diffTree : diffTree
+		mergeTrees : mergeTrees
 	}
-	
 });
