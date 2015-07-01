faba158ad0a606a946913a7a09e78f964375dd04
kxkx5150 <senna5150ando@gmail.com>
2015-07-01 16:00:17 +0900

    FileError  --> DomError

--- utils/errors.js
+++ utils/errors.js
@@ -73,7 +73,7 @@ define(function() {
             }
             return function(e) {
                 var msg = errors.getFileErrorMsg(e);
-                onError({type : errors.FILE_IO_ERROR, msg: msg, fe: e.code});
+                onError({type : errors.FILE_IO_ERROR, msg: msg, fe: e.name});
             }
         },
 
@@ -97,45 +97,45 @@ define(function() {
         getFileErrorMsg: function(e) {
             var msg = '';
 
-            switch (e.code) {
-                case FileError.QUOTA_EXCEEDED_ERR:
+            switch (e.name) {
+                case "QuotaExceededError":
                     msg = 'QUOTA_EXCEEDED_ERR';
                     break;
-                case FileError.NOT_FOUND_ERR:
+                case "NotFoundError":
                     msg = 'NOT_FOUND_ERR';
                     break;
-                case FileError.SECURITY_ERR:
+                case "SecurityError":
                     msg = 'SECURITY_ERR';
                     break;
-                case FileError.INVALID_MODIFICATION_ERR:
+                case "InvalidModificationError":
                     msg = 'INVALID_MODIFICATION_ERR';
                     break;
-                case FileError.INVALID_STATE_ERR:
+                case "InvalidStateError":
                     msg = 'INVALID_STATE_ERR';
                     break;
-                case FileError.ABORT_ERR:
+                case "AbortError":
                     msg = 'ABORT_ERR';
                     break;
-                case FileError.ENCODING_ERR:
+                case "EncodingError":
                     msg = 'ENCODING_ERR';
                     break;
-                case FileError.NOT_READABLE_ERR:
+                case "NotReadableError":
                     msg = 'NOT_READABLE_ERR';
                     break;
-                case FileError.NO_MODIFICATION_ALLOWED_ERR:
+                case "NoModificationAllowedError":
                     msg = 'NO_MODIFICATION_ALLOWED_ERR';
                     break;
-                case FileError.PATH_EXISTS_ERR:
+                case "PathExistsError":
                     msg = 'PATH_EXISTS_ERR';
                     break;
-                case FileError.SYNTAX_ERR:
+                case "SyntaxError":
                     msg = 'SYNTAX_ERR';
                     break;
-                case FileError.TYPE_MISMATCH_ERR:
+                case "TypeMismatchError":
                     msg = 'TYPE_MISMATCH_ERR';
                     break;
                 default:
-                    msg = 'Unknown Error ' + e.code;
+                    msg = 'Unknown Error ' + e.name;
                     break;
             };
         },
